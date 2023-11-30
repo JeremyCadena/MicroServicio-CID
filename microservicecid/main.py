7925bb462e2d2b7dc573096114fc29b0aa1d8a4e
JeremyCadena <jejocad2000@hotmail.com>
2023-11-30 09:43:59 -0500

    Aprobacion

--- microservicecid/main.py
+++ microservicecid/main.py
@@ -15,7 +15,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Configura la conexión a la base de datos
+# Configuración de la conexión a la base de datos
 database_config = {
     "host": "localhost",
     "user": "root",
@@ -30,7 +30,10 @@ cursor = conn.cursor(dictionary=True)
 clave_secreta = Fernet.generate_key()
 fernet = Fernet(clave_secreta)
 
-# Ruta para obtener todos los clientes
+@app.get("/")
+def inicio():
+    return {"message": "API en funcionamiento"}
+
 @app.get("/clientes/", response_model=list[dict])
 async def obtener_clientes():
     cursor.execute("SELECT * FROM clientes")
@@ -47,7 +50,6 @@ async def obtener_clientes():
 
     return clientes_cifrados
 
-# Ruta para obtener un cliente por ID
 @app.get("/clientes/{cliente_id}", response_model=dict)
 async def obtener_cliente(cliente_id: int):
     cursor.execute("SELECT * FROM clientes WHERE ID = %s", (cliente_id,))
@@ -63,3 +65,61 @@ async def obtener_cliente(cliente_id: int):
     }
 
     return cliente_descifrado
+
+# Ruta para agregar un nuevo cliente mediante POST
+@app.post("/clientes/", response_model=dict)
+async def agregar_cliente(cliente: dict):
+    # Descifra los valores del cliente antes de agregarlos a la base de datos
+    cliente_descifrado = {
+        key: fernet.decrypt(str(value).encode()).decode() if key != "ID" else value
+        for key, value in cliente.items()
+    }
+
+    # Inserta el nuevo cliente en la base de datos
+    cursor.execute(
+        "INSERT INTO clientes (Nombre, Apellido, Direccion, Ciudad, Email, Telefono, Ocupacion) "
+        "VALUES (%(Nombre)s, %(Apellido)s, %(Direccion)s, %(Ciudad)s, %(Email)s, %(Telefono)s, %(Ocupacion)s)",
+        cliente_descifrado,
+    )
+    conn.commit()
+
+    # Devuelve el cliente recién agregado
+    return cliente_descifrado
+
+# Ruta para actualizar un cliente por ID mediante PUT
+@app.put("/clientes/{cliente_id}", response_model=dict)
+async def actualizar_cliente(cliente_id: int, cliente_actualizado: dict):
+    # Descifra los valores del cliente antes de actualizarlos en la base de datos
+    cliente_descifrado = {
+        key: fernet.decrypt(str(value).encode()).decode() if key != "ID" else value
+        for key, value in cliente_actualizado.items()
+    }
+
+    # Actualiza el cliente en la base de datos
+    cursor.execute(
+        "UPDATE clientes SET Nombre = %(Nombre)s, Apellido = %(Apellido)s, Direccion = %(Direccion)s, "
+        "Ciudad = %(Ciudad)s, Email = %(Email)s, Telefono = %(Telefono)s, Ocupacion = %(Ocupacion)s "
+        "WHERE ID = %(ID)s",
+        {"ID": cliente_id, **cliente_descifrado},
+    )
+    conn.commit()
+
+    # Devuelve el cliente actualizado
+    return cliente_descifrado
+
+# Ruta para eliminar un cliente por ID mediante DELETE
+@app.delete("/clientes/{cliente_id}", response_model=dict)
+async def eliminar_cliente(cliente_id: int):
+    # Obtiene el cliente antes de eliminarlo
+    cursor.execute("SELECT * FROM clientes WHERE ID = %s", (cliente_id,))
+    cliente = cursor.fetchone()
+
+    if not cliente:
+        raise HTTPException(status_code=404, detail="Cliente no encontrado")
+
+    # Elimina el cliente de la base de datos
+    cursor.execute("DELETE FROM clientes WHERE ID = %s", (cliente_id,))
+    conn.commit()
+
+    # Devuelve el cliente eliminado
+    return cliente
